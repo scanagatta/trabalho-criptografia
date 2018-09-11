101371c7c72b0aaffeb65589745e2eaaf60aca13
ander <ander@172.18.24.14>
2018-09-10 21:14:05 -0300

    mudando umas coisas

--- vraptor-blank-project/src/main/java/br/com/caelum/vraptor/controller/IndexController.java
+++ vraptor-blank-project/src/main/java/br/com/caelum/vraptor/controller/IndexController.java
@@ -4,11 +4,14 @@ import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
+import model.CifraCesar;
 
 @Controller
 @Path("/index")
 public class IndexController {
 
+	CifraCesar cifraCesar = new CifraCesar();
+	
 	@Get("/index")
 	public void index() {
 
@@ -16,7 +19,7 @@ public class IndexController {
 
 	@Post("/criptografar")
 	public void criptografar(String texto1) {
-
+		
 	}
 
 	@Post("/descriptografar")
